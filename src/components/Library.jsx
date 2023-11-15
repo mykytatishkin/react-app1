dc74cd4eac595cd239efce9c4d1367b7788cc60c
Mykyta Tishkin <87891550+GTeasera@users.noreply.github.com>
2023-11-15 18:19:04 +0200

    Data binding and handlers

--- src/components/Library.jsx
+++ src/components/Library.jsx
@@ -11,11 +11,12 @@ class Library extends Component {
         // this.state.count = this.state.count + 1; // wrong way to change state, never use it
         this.setState(
             (oldState) => { return { count: oldState.count + 1} },
+
             () => { console.log('count = ' + this.state.count) }
             );
         // console.log('count = ' + this.state.count)
     }
-    
+/* Not effective way   
     handleTitle = (event) => {
     this.setState( { title: event.target.value }, () => {console.log(this.state)} );
     }
@@ -23,6 +24,25 @@ class Library extends Component {
     handleAuthor = (event) => {
         this.setState( { author: event.target.value }, () => {console.log(this.state)} );
         }
+*/
+    // Better way than previous 
+    //
+    // handleInput = event => {
+    //     const name = event.target.name;
+    //     const obj = {};
+    //     obj[name] = event.target.value;
+    //     this.setState(obj);
+    //     console.log(this.state);
+    // }
+
+    //spread  
+    handleSubmit = (event) => {    
+        event.preventDefault();    
+        const book = { title: this.state.title, author: this.state.author };    
+        //this.setState( (prev) => { books: prev.books.push(book) }, () => { console.log(this.state.books) } );    
+        //this.setState( (prev) => ({ books: [...prev.books, book] }), () => { console.log(this.state.books) } );    
+        //this.setState( () => ({ books: [...this.state.books, book] }), () => { console.log(this.state.books) } );    
+        this.setState( { books: [...this.state.books, book] }, () => { console.log(this.state.books) } );  }
 
     render() {
         return (
@@ -32,11 +52,28 @@ class Library extends Component {
                 <p>Current number of books: {this.state.count}</p>
                 <hr/>
                 {/* Add new book: <button onClick={this.handlerClick}>Add book</button> */}
-                <form>
-                    <input type='text' name='title' placeholder='Title' onChange={this.handleTitle}/>
-                    <input type='text' name='author' placeholder='Author' onChange={this.handleAuthor}/>
+                <form onSubmit={this.handleSubmit}>
+                    {/* <input type='text' name='title' placeholder='Title' onChange={this.handleInput}/> */}
+                    {/* <input type='text' name='author' placeholder='Author' onChange={this.handleInput}/> */}
+                    
+                    <input type='text' name='title' placeholder='Title' onChange={ (e) => this.setState( { title: e.target.value } )}/>
+                    <input type='text' name='author' placeholder='Author'onChange={ (e) => this.setState( { author: e.target.value } )}/>
                     <button type='submit'> Add Book</button>
                 </form>
+                <hr/>
+                <table className='table table-striped'>
+                    <thead>
+                        <tr>
+                            <th>Title</th>
+                            <th>Author</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            this.state.books.map( (b, i) => <tr key={i}><td>{ b.title }</td><td>{ b.author }</td></tr>)
+                        }
+                    </tbody>
+                </table>
             </div>
         );
     }
